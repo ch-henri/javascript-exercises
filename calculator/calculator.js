1c0be3b60af496677d2c5c0ebdb09c2ff319ddb6
charles-henri <charles-henri.deschenaux@devfactory.ch>
2021-05-30 21:30:43 +0200

    Done exercise

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,6 +1,16 @@
-function add () {
-	
+function add (numbers) {
+	let args = Array.from(arguments);
+  args.filter(item => { if(typeof item !== 'number')  return 'ERROR' } );
+
+  let sum = args.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  });
+
+  console.log(sum);
+  
 }
+
+add(5, 6,56,6,34,5)
 
 function subtract () {
 	
